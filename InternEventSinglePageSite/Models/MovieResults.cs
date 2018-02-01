a015c0faaf0b2c25c249eeb83ff6ab87540193af
Asa Jenkins <asa4jenkins@gmail.com>
2018-02-01 09:48:26 -0500

    Added display names and carousel.

--- InternEventSinglePageSite/Models/MovieResults.cs
+++ InternEventSinglePageSite/Models/MovieResults.cs
@@ -1,20 +1,30 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace InternEventSinglePageSite.Models
 {
     public class MovieResults : Movie
     {
+        
         public int vote_count { get; set; }
         public long id { get; set; }
+
+        [DisplayName("Rating")]
         public long vote_average { get; set; }
+
+        [DisplayName("Title")]
         public string title { get; set; }
         public string poster_path { get; set; }
         public bool adult { get; set; }
+
+        [DisplayName("Overview")]
         public string overview { get; set; }
 
+        [DisplayName("Release Date")]
         [DataType(DataType.Date)]
         public DateTime release_date { get; set; }
+
         public string DOR { get; set; }
     }
 }
